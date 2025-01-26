14b9bea8ee78e4fb4b513a611ebfc28dfebb58a2
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2025-01-26 18:23:26 +0530

    Navbar and Banner

---
+++ src/components/section/Home/mainHome.js
@@ -0,0 +1,13 @@
+import Banner from '@/components/common/MainBanner/Banner'
+import Pagewrapper from '@/components/layout/pagewrapper'
+import React from 'react'
+
+function MainHome() {
+  return (
+    <Pagewrapper>
+        <Banner/>
+    </Pagewrapper>
+  )
+}
+
+export default MainHome
